3002fdb21a58287e10933437bfc92dc2342c6078
Andrea Ligorio <andrea85kro@gmail.com>
2021-03-02 17:48:12 +0100

    Integrato html e css

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
+function myFunction() {
 const pcRandomNumber = 16;
 
 var difficulty;
 var flag = false;
 while (flag == false) {
-    var difficultyChoise = prompt("Inserisci la difficoltà:\n0 : Facile\n1 : Medio\n2 : Difficile");
+    var difficultyChoise = document.getElementById("level").value;
+    var minefield;
+    var minefieldHard;
     switch (difficultyChoise) {
-        case "0":
+        case "easy":
             difficulty = 100;
             flag = true;
             break;
-        case "1":
+        case "medium":
             difficulty = 80;
+            minefield = document.getElementById("hard");
+            minefield.className = "hidden";
             flag = true;
             break;
-        case "2":
+        case "hard":
             difficulty = 50;
+            minefield = document.getElementById("medium");
+            minefield.className = "hidden";
+            minefieldHard = document.getElementById("hard");
+            minefieldHard.className = "hidden";
             flag = true;
             break;
-        default:
-            alert("Devi inserire un numero da 0 a 2!");
-    }
+            
+        }
 }
 var pcArrayNumber = [];
 var userNumber; 
 var arrayUser = [];
 var punteggio;
+var box;
+
 
 while (pcArrayNumber.length < pcRandomNumber) {
     var pcNumber = randomNumber(1, difficulty);
@@ -40,45 +50,61 @@ console.log(pcArrayNumber);
 while ((!isInArray(pcArrayNumber, userNumber)) && ((arrayUser.length) < (difficulty - pcRandomNumber))) {
     flag = false;
     while (flag == false) {
-        userNumber = prompt("Inserisci un numero da 1 a " + difficulty);
-        if (isNaN(userNumber) || (userNumber.includes("."))) {
-            alert("inserisci un numero!!!")
-        } else if ((userNumber < 1) || (userNumber > difficulty)) {
-            alert("Inserisci un numero compreso tra 1 e " + difficulty);
+            userNumber = prompt("Inserisci un numero da 1 a " + difficulty);
+            if (isNaN(userNumber) || (userNumber.includes("."))) {
+                alert("inserisci un numero!!!");
+            } else if ((userNumber < 1) || (userNumber > difficulty)) {
+                alert("Inserisci un numero compreso tra 1 e " + difficulty);
+            } else {
+                flag = true;
+            }
+        }
+        if (!isInArray(arrayUser, userNumber)) {
+            if (!isInArray(pcArrayNumber, userNumber)) {
+                arrayUser.push(userNumber);
+                box = document.getElementById("box" + userNumber);
+                box.className = "check";
+            }
+            else {
+                box = document.getElementById("box" + userNumber);
+                box.className = "mine";
+            }
         } else {
-            flag = true;
+            alert("Numero già inserito... ripetere!");
         }
-    }
-    if (!isInArray(arrayUser, userNumber)) {
-        if (!isInArray(pcArrayNumber, userNumber)) {
-            arrayUser.push(userNumber);
+}
+    
+    punteggio = arrayUser.length;
+    
+    /* Con un if controllo se è stato raggiunto il punteggio massimo o è stato pescato un numero perdente */
+    if ((!isInArray(pcArrayNumber, arrayUser[arrayUser.length])) && arrayUser.length == (difficulty - pcRandomNumber)) {
+        for(var i=0; i < pcArrayNumber.length; i++) {
+            box = document.getElementById("box" + pcArrayNumber[i]);
+            box.className = "mine";
         }
+        document.getElementById("result").innerHTML = "Complimenti hai fatto il punteggio massimo: " + arrayUser.length + " punti.";
     } else {
-        alert("Numero già inserito... ripetere!");
-    }
-}
-
-punteggio = arrayUser.length;
-
-/* Con un if controllo se è stato raggiunto il punteggio massimo o è stato pescato un numero perdente */
-if ((!isInArray(pcArrayNumber, arrayUser[arrayUser.length])) && arrayUser.length == (difficulty - pcRandomNumber)) {
-    alert("Complimenti hai fatto il punteggio massimo: " + arrayUser.length + " punti\nI numeri perdenti erano " + pcArrayNumber);
-} else {
-    alert("La partita è terminata ed hai fatto " + punteggio + " punti.\nPurtroppo il numero " + userNumber + " era un numero perdente\nI numeri perdenti erano " + pcArrayNumber);
-}
-
-console.log(arrayUser);
-console.log(punteggio);
-
-function isInArray(array, value) {
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] == value) {
-            return true;
+        for (var i = 0; i < pcArrayNumber.length; i++) {
+            box = document.getElementById("box" + pcArrayNumber[i]);
+            box.className = "mine";
         }
+        document.getElementById("result").innerHTML = "La partita è terminata ed hai fatto " + punteggio + " punti.";
     }
-    return false;
-}
-
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    
+    console.log(arrayUser);
+    console.log(punteggio);
+    
+    function isInArray(array, value) {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    
 }
